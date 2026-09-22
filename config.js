5c43bf1914ab378e7c7caa80dda378290141c34d
wearegatheo-netizen <wearegatheo@gmail.com>
2026-09-22 11:54:54 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 //   supabaseAnonKey : 같은 화면의 anon public 키 (게더 올 어라운드 사이트 index.html의 SUPABASE_URL / SUPABASE_KEY와 동일하게)
 // 비워 두면 클라우드 버튼만 꺼지고 나머지 기능은 그대로 동작합니다.
 window.CAROUSEL_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://xdcygyvsybsirmaknmpq.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkY3lneXZzeWJzaXJtYWtubXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNTMxNTIsImV4cCI6MjA5MzcyOTE1Mn0.KknBzg2OfcsVawxp0BTBJuwXMWX6tNZo-6nCm64nGeI',
   table: 'carousel_projects'
 };
